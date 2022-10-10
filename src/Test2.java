8ca0cc733ec39f0114212eeef2adcf388d9a460c
user <user@Sujay>
2022-10-10 18:09:13 +0530

    Commiting Test2 to BranchX

---
+++ src/Test2.java
@@ -0,0 +1,17 @@
+
+public class Test2 
+{
+
+	public void m1() 
+	{
+		System.out.println("M!_A");
+	}
+	
+	public static void main(String[] args) {
+		
+		Test2 test=new Test2();
+		     test.m1();
+		  
+	}
+}
+
