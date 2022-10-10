eeb9c2e67c8acc47795211d2099097dd07fec965
user <user@Sujay>
2022-10-10 18:23:43 +0530

    changes are made in code

--- src/Test2.java
+++ src/Test2.java
@@ -6,11 +6,16 @@ public class Test2
 	{
 		System.out.println("M!_A");
 	}
+	public void m2() 
+	{
+		System.out.println("M!_S");
+	}
 	
 	public static void main(String[] args) {
 		
 		Test2 test=new Test2();
 		     test.m1();
+		     test.m2();
 		  
 	}
 }
