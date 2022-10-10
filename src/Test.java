61fa0ce5de26594dffadeff0e400d78c13705fa8
vaish <vaish@Vaishnavi>
2022-10-10 17:10:48 +0530

    By Master

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test 
+{
+
+	public void m1() 
+	{
+		System.out.println("M!_A");
+	}
+	
+	public static void main(String[] args) {
+		
+		Test test=new Test();
+		     test.m1();
+		  
+	}
+}
+
